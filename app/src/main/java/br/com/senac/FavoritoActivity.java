3ae5e84d5b340dd8e79ccf0fe2359a26e62a313c
William Bigas Mauro <williambmauro@hotmail.com>
2020-06-16 15:54:31 -0300

    Adicionado FavoritoActivity

---
+++ app/src/main/java/br/com/senac/FavoritoActivity.java
@@ -0,0 +1,14 @@
+package br.com.senac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoritoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorito);
+    }
+}
