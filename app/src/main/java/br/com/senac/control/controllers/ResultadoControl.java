42753c98c4bd746fb2b8d7f6fd035ba69b396c0a
LuMistro <luiza.wmistro@gmail.com>
2020-06-17 16:48:31 -0300

    control da tela Resultado iniciado

---
+++ app/src/main/java/br/com/senac/control/controllers/ResultadoControl.java
@@ -0,0 +1,9 @@
+package br.com.senac.control.controllers;
+
+import br.com.senac.view.Resultado;
+
+public class ResultadoControl {
+
+    public ResultadoControl(Resultado view) {
+    }
+}
--- app/src/main/java/br/com/senac/view/Resultado.java
+++ app/src/main/java/br/com/senac/view/Resultado.java
@@ -1,16 +1,46 @@
 package br.com.senac.view;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import br.com.senac.R;
+import br.com.senac.control.controllers.ResultadoControl;
 
 public class Resultado extends AppCompatActivity {
+
+    private TextView tvCidade;
+    private Button btnFavorito;
+    private ImageView imgClima;
+    private TextView tvClimaDesc;
+    private TextView tvTemp;
+    private TextView tvUmidade;
+
+    private Resultado resultado;
+
+    ResultadoControl control;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        inicializaComponentes();
+        control = new ResultadoControl(this.resultado);
     }
+
+    private void inicializaComponentes() {
+        tvCidade = findViewById(R.id.tvCidade);
+        btnFavorito = findViewById(R.id.btnFavorito);
+        imgClima = findViewById(R.id.imgClima);
+        tvClimaDesc = findViewById(R.id.tvClimaDesc);
+        tvTemp = findViewById(R.id.tvTemp);
+        tvUmidade = findViewById(R.id.tvUmidade);
+
+        resultado = new Resultado();
+    }
+
+
 }
