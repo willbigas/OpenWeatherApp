135f1214e944b9cb5480590e3c3edab948b8177c
William Bigas Mauro <williambmauro@hotmail.com>
2020-06-18 11:35:52 -0300

    Adicionado mais icones dinamicos na view

--- app/src/main/java/br/com/senac/control/controllers/ResultadoControl.java
+++ app/src/main/java/br/com/senac/control/controllers/ResultadoControl.java
@@ -33,29 +33,56 @@ public class ResultadoControl {
         if (nomeDaImagem.endsWith("01d")) {
             return R.drawable.i01d;
         }
+        if (nomeDaImagem.endsWith("01n")) {
+            return R.drawable.i01n;
+        }
         if (nomeDaImagem.endsWith("02d")) {
             return R.drawable.i02d;
+        }
+        if (nomeDaImagem.endsWith("02n")) {
+            return R.drawable.i02n;
         }
         if (nomeDaImagem.endsWith("03d")) {
             return R.drawable.i03d;
         }
+        if (nomeDaImagem.endsWith("03n")) {
+            return R.drawable.i03n;
+        }
         if (nomeDaImagem.endsWith("04d")) {
             return R.drawable.i04d;
+        }
+        if (nomeDaImagem.endsWith("04n")) {
+            return R.drawable.i04n;
         }
         if (nomeDaImagem.endsWith("09d")) {
             return R.drawable.i09d;
         }
+        if (nomeDaImagem.endsWith("09n")) {
+            return R.drawable.i09n;
+        }
         if (nomeDaImagem.endsWith("10d")) {
             return R.drawable.i10d;
+        }
+        if (nomeDaImagem.endsWith("10n")) {
+            return R.drawable.i10n;
         }
         if (nomeDaImagem.endsWith("11d")) {
             return R.drawable.i11d;
         }
+        if (nomeDaImagem.endsWith("11n")) {
+            return R.drawable.i11n;
+        }
         if (nomeDaImagem.endsWith("13d")) {
             return R.drawable.i13d;
         }
+        if (nomeDaImagem.endsWith("13n")) {
+            return R.drawable.i13n;
+        }
         if (nomeDaImagem.endsWith("50d")) {
             return R.drawable.i50d;
+        }
+        if (nomeDaImagem.endsWith("50n")) {
+            return R.drawable.i50n;
         }
         return 0;
     }
